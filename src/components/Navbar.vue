a66f3cb58eca8c68ead6f93795f1fb81a86bbed0
ljutaev <ljutaev@gmail.com>
2022-02-02 23:24:32 +0200

    routes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+	<nav class="navbar">
+		<router-link to="/">Home</router-link>
+		<router-link to="/about">About</router-link>
+		<router-link to="/contacts">Contacts</router-link>
+	</nav>
+</template>
+
+<script>
+	export default {
+		name: 'Navbar',
+	}
+</script>
+
+<style>
+	
+</style>
